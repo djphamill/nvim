923d34c3570a4fe7f6f9502accf4090edca82e1c
David <djphamill@gmail.com>
2024-08-15 15:49:01 +0100

    Automatically close brackets and quotes.

--- init.lua
+++ init.lua
@@ -103,6 +103,13 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Automatically close parentheses and quotes
+vim.keymap.set("i", "{", "{}<Esc>ha", { desc = "Automatically close curly brace" })
+vim.keymap.set("i", "[", "[]<Esc>ha", { desc = "Automatically close square bracket" })
+vim.keymap.set("i", "(", "()<Esc>ha", { desc = "Automatically close bracket" })
+vim.keymap.set("i", "'", "''<Esc>ha", { desc = "Automatically close single quote" })
+vim.keymap.set("i", '"', '""<Esc>ha', { desc = "Automatically close double quote" })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
